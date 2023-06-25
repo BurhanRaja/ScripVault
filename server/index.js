6d11f0ed3b277bf4ecb376e8fba549de740b1823
BurhanRaja <burhanraja02@yahoo.com>
2023-06-25 10:19:45 +0530

    routes added

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import portfolio from "./routes/portfolio.js";
+import user from "./routes/user.js";
+import watchlist from "./routes/watchlist.js";
 
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-
+app.use("/api/portfolio", portfolio);
+app.use("/api/user", user);
+app.use("/api/watchlist", watchlist);
 
 app.listen(port, () => {
-    console.log(`App started at http://localhost:${port}`)
-})
+  console.log(`App started at http://localhost:${port}`);
+});
