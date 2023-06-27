8ea6a59464aa819fb9231b6c13cfdf03f6ebd167
BurhanRaja <burhanraja02@yahoo.com>
2023-06-27 11:55:54 +0530

    app started and setup

--- server/controller/stocks.js
+++ server/controller/stocks.js
@@ -1,2 +1,17 @@
 // search stocks
 // search Mutual fund
+
+// Get All Stocks NSE
+// Get All Stocks BSE
+// Get Stock Current Price
+
+// Financial Ratios
+// Revenue Statement Graph
+// Balance Sheet Stmt Graph
+// Cash Flow Stmt Graph
+
+// historical Data
+// Stock Info
+// Cash Flows
+// Balance Sheet
+// Income Stmt
