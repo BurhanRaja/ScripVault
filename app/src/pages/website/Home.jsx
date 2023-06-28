fc3c2443987103ae7aa80d534f7ebc6246211171
BurhanuddinRaja <mails4burhan@gmail.com>
2023-06-28 13:30:50 +0530

    added react-icons and home page ready

--- app/src/pages/website/Home.jsx
+++ app/src/pages/website/Home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Hero from "../../components/website/Hero";
 import InvestmentOptionSection from "../../components/website/InvestmentOptionSection";
 import FeaturesSection from "../../components/website/FeaturesSection";
+import Header from "../../components/website/Header";
+import Footer from "../../components/website/Footer";
 
 const Home = () => {
   return (
     <>
+      <Header />
       <Hero />
       <FeaturesSection />
       <InvestmentOptionSection />
+      <Footer />
     </>
   );
 };
