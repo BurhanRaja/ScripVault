0c6bd1842535be011cde8e56c5e112bca709d881
BurhanRaja <burhanraja02@yahoo.com>
2023-06-29 21:26:50 +0530

    store, login and register added

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/website/Home";
+import Login from "./pages/website/Login";
+import Register from "./pages/website/Register";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" />
-          <Route path="/register" />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
